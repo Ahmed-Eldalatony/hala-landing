fc7cbf94b74ef57292f16245635133bbc7b27e03
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-11-19 08:55:11 +0200

    host is true now

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
+    host: true,
     allowedHosts: [
       "halakommers.com",
       "seller.halakommers.com",
